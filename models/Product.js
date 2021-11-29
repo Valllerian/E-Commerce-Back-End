c03dd0cbf05b9552b9efdde8c8d46ad3ee3a3b12
Valerii Bihun <valeriibihun.co@gmail.com>
2021-11-28 22:16:46 -0700

    ProductJS written

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,42 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // defining columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull:false,
+      // Validates that the value is a decimal;
+      validate:{
+        isDecimal:true,
+      },
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue:10,
+      // Validates that the value is numeric;
+      validate:{
+        isNumeric:true,
+      },
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      // References the Category model`s id;
+      references:{
+        model:"category",
+        key:"id",
+      },
+    },
   },
   {
     sequelize,
